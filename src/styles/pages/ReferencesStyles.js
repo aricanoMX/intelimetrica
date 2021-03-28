fa0ce4da0439c1aa7797dc9bbbc559250120c2d3
AricanoMX <aricanomx@gmail.com>
2021-03-28 06:37:25 -0600

    [add] firebase hosting

--- src/styles/pages/ReferencesStyles.js
+++ src/styles/pages/ReferencesStyles.js
@@ -5,11 +5,7 @@ import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 export const StyledReferences = styled.main`
   display: flex;
   flex-direction: column;
-  /* display: grid;
-  grid-template-rows: 0px 50% 50%;
-  grid-template-areas:
-    'all-tags'
-    'recomendations'; */
+
   padding-bottom: 1rem;
 `;
 export const LoadMap = styled(LoadScript)`
@@ -26,5 +22,5 @@ export const ComboboxStyles = styled.div`
   width: 100%;
   height: 25px;
   width: 300px;
-  z-index: 9;
+  z-index: 8;
 `;
